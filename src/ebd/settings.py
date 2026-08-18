fd90427c8d819cc5a9accecc4fc950171500a971
Joao Barbosa da Silva <iotabe@iotabe.adm.br>
2026-08-18 20:08:09 -0300

    storage

--- src/ebd/settings.py
+++ src/ebd/settings.py
@@ -149,3 +149,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']    
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
